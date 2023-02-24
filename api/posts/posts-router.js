15f72e96000d883007f28737046f2db93be61fc8
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-02-23 23:37:44 -0500

    API can handle the first two get routes

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,58 @@
 // implement your posts router here
+const express = require('express');
+const Post = require('./posts-model');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    Post.find(req.query)
+        .then(posts => {
+            res.status(200).json(posts);
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "The posts information could not be retrieved"
+            });
+        })
+});
+
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    Post.findById(id)
+        .then(post => {
+            if (!post) {
+                res.status(404).json({
+                    message: "The post with the specified ID does not exist"
+                });
+            } else {
+                res.status(200).json(post);
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "The post information could not be retrieved"
+            });
+        })
+});
+
+//FIX 
+router.post('/', (req, res) => {
+    Post.insert(req.body)
+        .then(post => {
+            if (!title || !contents) {
+                res.status(400).json({
+                    message: "Please provide title and contents for the post"
+                });
+            } else {
+                res.status(201).json(post);
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "There was an error while saving the post to the database"
+            });
+        })
+});
+
+module.exports = router;
