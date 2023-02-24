45de0caed60174c2caa76a688c5c413a27c1d2f3
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-02-24 00:00:13 -0500

    configured the API to handle the post route

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -36,21 +36,49 @@ router.get('/:id', (req, res) => {
         })
 });
 
-//FIX 
+
 router.post('/', (req, res) => {
-    Post.insert(req.body)
+    const { title, contents } = req.body;
+    if (!title || !contents) {
+        res.status(400).json({
+            message: "Please provide title and contents for the post"
+        });
+    } else {
+        Post.insert({ title, contents })
+            .then(({ id }) => {
+                return Post.findById(id);
+            })
+            .then(post => {
+                res.status(201).json(post);
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: "There was an error while saving the post to the database"
+                });
+            })
+    }
+});
+
+
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    Post.update(id, req.body) 
         .then(post => {
-            if (!title || !contents) {
+            if (!post) {
+                res.status(404).json({
+                    message: "The post with the specified ID does not exist"
+                })
+            } else if (!title || !contents) {
                 res.status(400).json({
                     message: "Please provide title and contents for the post"
-                });
+                })
             } else {
-                res.status(201).json(post);
+                res.status(200).json(post)
             }
         })
         .catch(err => {
             res.status(500).json({
-                message: "There was an error while saving the post to the database"
+                message: "The post information could not be modified"
             });
         })
 });
